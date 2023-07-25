de39a58e1cdc74730a6113fbad40519fce685e43
marveltaylor <marveltaylor85@gmail.com>
2023-07-25 14:42:00 -0700

    mounted Authenticate and SignUpForm components inside jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import SignUpForm from './components/SignUpForm'
 import Authenticate from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <Authenticate />
+    <SignUpForm />
     </>
   )
-}
+};
 
 export default App
