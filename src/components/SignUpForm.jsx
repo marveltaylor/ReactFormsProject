d2654e950d071136584c9cb67c41d8cfcb6f20e6
marveltaylor <marveltaylor85@gmail.com>
2023-07-25 14:50:30 -0700

    created state variables username, password, and error

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
-export default function SignUpForm () {
+const [username, setUsername] = useState(""); //create username state variable with value ""
+const [password, setPassword] = useState(""); //create password state variable with value ""
+const [error, setError] = useState(null); //create error state variable with value null
+
+export default function SignUpForm () { //create SignUpForm component
     return (
         <h2>Sign Up!</h2>
     )
