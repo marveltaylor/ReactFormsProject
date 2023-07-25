a35a6456ac15f1c8899b04743998d4b674e240a3
marveltaylor <marveltaylor85@gmail.com>
2023-07-25 14:38:10 -0700

    imported SignUpForm and Authenticate components to App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
 
 function App() {
   const [count, setCount] = useState(0)
