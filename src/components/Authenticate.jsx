90078e08266a07719cde3cfd3c68f62cea82c715
marveltaylor <marveltaylor85@gmail.com>
2023-07-25 17:03:05 -0700

    created onClick button

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,5 +1,9 @@
 export default function Authenticate (token) { //create Authenticate component 
     return (
+    <>
         <h2>Authenticate!</h2> 
+        <button onClick={handleClick}>Authenticate Token!</button>
+    </>
+
     )
 }
