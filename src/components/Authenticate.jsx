aa266f38b262aea355c047377e569edba7f6d2a0
marveltaylor <marveltaylor85@gmail.com>
2023-07-25 17:18:06 -0700

    created error and setError state variables initializing their values to null

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 export default function Authenticate (token) { //create Authenticate component 
+    const [error, setError] = useState(null);
+    
     async function handleClick() {
         try {
         } catch (error) {
